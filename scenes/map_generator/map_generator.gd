e1523bb12d927afd8b3e2ed00f0d338a91a74510
Micael Brito <micael@oaksgamestudio.com>
2024-03-13 10:53:19 -0300

    Upload project and add initial ap logic

---
+++ scenes/map_generator/map_generator.gd
@@ -0,0 +1,37 @@
+extends Node3D
+
+@export var size: Vector2i
+@export var tile_scene: PackedScene
+
+func _ready():
+	_set_map()
+	
+
+func _set_map():
+	_recenter_map()
+	_create_path()
+	
+
+func _recenter_map():
+	var map_x = (size.x / 2) * -1
+	var map_y = (size.y / 2) * -1
+	var map_position = Vector3i(map_x, 0, map_y)
+	
+	self.position = map_position
+	
+
+func _create_path():
+	for x in size.x:
+		for y in size.y:
+			var coord = Vector2i(x, y)
+			_fill(coord)
+			print(str(coord))
+	
+
+func _fill(coord: Vector2i):
+	var position = Vector3i(coord.x, 0, coord.y)
+	
+	var tile = tile_scene.instantiate()
+	add_child(tile)
+	tile.position = position
+	
