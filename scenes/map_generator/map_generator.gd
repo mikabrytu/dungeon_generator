076e7301fabc38d807a851b2a63a732ccceb3c83
Mikabrytu <mikabrytu@gmail.com>
2024-03-13 12:05:11 -0300

    Add direct path algorithm

--- scenes/map_generator/map_generator.gd
+++ scenes/map_generator/map_generator.gd
@@ -21,14 +21,44 @@ func _recenter_map():
 	
 
 func _create_path():
-	print("Calc new path to end level")
-	pass
+	var start_tile = Vector2i(randi_range(0, size.x - 1), 0)
+	var end_tile = Vector2i(randi_range(0, size.x - 1), size.y -1)
+	
+	_fill(start_tile, Color.GREEN)
+	_fill(end_tile, Color.RED)
+	
+	var current_coord = start_tile
+	var axis = randi_range(0, 1)
+	
+	while current_coord != end_tile:
+		var destination = end_tile.x if axis == 0 else end_tile.y
+		var current = current_coord.x if axis == 0 else current_coord.y
+		var remaining = destination - current
+		var direction = 1 if remaining > 0 else -1
+		
+		if (remaining == 0):
+			axis = 1 if axis == 0 else 0
+			continue
+		
+		if axis == 0:
+			current_coord.x += direction
+		elif axis ==  1:
+			current_coord.y += direction
+		
+		if (current_coord != end_tile):
+			_fill(current_coord, Color.BLUE)
 	
 
-func _fill(coord: Vector2i):
+func _fill(coord: Vector2i, color: Color = Color.WHITE):
 	var position = Vector3i(coord.x, 0, coord.y)
 	
 	var tile = tile_scene.instantiate()
 	add_child(tile)
 	tile.position = position
+	
+	if (color != Color.WHITE):
+		var material = StandardMaterial3D.new()
+		material.albedo_color = color
+		
+		tile.get_node("Model").set_material(material)
 	
