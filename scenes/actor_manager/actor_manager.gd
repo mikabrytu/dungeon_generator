37ec6948e62c698d87fdd8d63012a5fcb06872f9
Mikabrytu <mikabrytu@gmail.com>
2024-03-21 14:23:30 -0300

    Listen to actors finishing their actions and adding a delay to next one to avoid infinite loops

--- scenes/actor_manager/actor_manager.gd
+++ scenes/actor_manager/actor_manager.gd
@@ -1,19 +1,33 @@
 extends Node3D
 
+@onready var next_turn_delay: Timer = $"Next Turn Delay"
+
 var actors: Array[Actor]
 var current_index: int = 0
 
+#region Godot Lifecycle
+
 func _ready():
-	_get_actors()
+	next_turn_delay.timeout.connect(_on_delay_timeout)
 	
+	_get_actors()
+	_call_next_actor()
+	
+
+#endregion
+
+#region Implementation
 
 func _get_actors():
 	var nodes = get_tree().get_nodes_in_group("actors")
 	
 	for node in nodes:
-		actors.append(node as Actor)
+		var actor = node as Actor
+		actor.actor_finished.connect(_on_actor_finished)
+		
+		actors.append(actor)
 	
-	actors.sort_custom(_sort_actors)
+	actors.sort_custom(func(a, b): return a.priority < b.priority)
 	print(actors)
 	
 
@@ -22,8 +36,18 @@ func _call_next_actor():
 		current_index = 0
 	
 	actors[current_index].can_act()
-	current_index += 1
+	current_index = current_index + 1
 	
 
-func _sort_actors(a, b):
-	return a.priority < b.priority
+#endregion
+
+#region Listeners
+
+func _on_actor_finished():
+	next_turn_delay.start()
+	
+
+func _on_delay_timeout():
+	_call_next_actor()
+
+#endregion
