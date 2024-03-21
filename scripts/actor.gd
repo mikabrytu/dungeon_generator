b2c6f34bc60fc1f7b0e5c9f3c809f470c9590c7d
Mikabrytu <mikabrytu@gmail.com>
2024-03-21 14:22:11 -0300

    Add actor signal for when actor finishes its action

--- scripts/actor.gd
+++ scripts/actor.gd
@@ -4,6 +4,9 @@ class_name Actor
 
 @export var priority: int = 1
 
+signal actor_finished
+
+#region Public API
 
 func get_priority() -> int:
 	return priority
@@ -11,3 +14,5 @@ func get_priority() -> int:
 
 func can_act():
 	pass
+
+#endregion
