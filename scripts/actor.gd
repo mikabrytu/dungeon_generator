4f2323ba403cc370f50299866a5079c14afc0975
Mikabrytu <mikabrytu@gmail.com>
2024-03-22 19:47:57 -0300

    Add Area3D for collision detection on Player and Actor.gd

--- scripts/actor.gd
+++ scripts/actor.gd
@@ -1,8 +1,20 @@
 class_name Actor extends Node3D
 
+const AttackTypes = preload("res://scripts/attack_types.gd")
+
 @export var priority: int = 1
+@export var weakness: AttackTypes.TYPES
+
+@onready var collision_area: Area3D = $Area3D
 
 signal actor_finished
+
+#region Godot Lifecycle
+
+func _ready():
+	collision_area.body_entered.connect(_on_body_entered)
+
+#endregion
 
 #region Public API
 
@@ -12,5 +24,12 @@ func get_priority() -> int:
 
 func can_act():
 	pass
+
+#endregion
+
+#region Listeners
+
+func _on_body_entered(body: Node3D):
+	print(body.name + " is hitting " + self.name)
 
 #endregion
