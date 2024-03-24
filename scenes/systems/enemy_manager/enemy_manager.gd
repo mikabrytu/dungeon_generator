8e555d6d0f8a5b33bf74a43751241f4c11084368
Mikabrytu <mikabrytu@gmail.com>
2024-03-24 01:27:26 -0300

    Set a max enemy count logic

--- scenes/systems/enemy_manager/enemy_manager.gd
+++ scenes/systems/enemy_manager/enemy_manager.gd
@@ -1,11 +1,16 @@
 extends Node3D
 
 @export var enemie_scenes: Array[PackedScene]
+@export var enemy_ratio: int
 @export var enemy_count: int
+@export var enemy_count_override: bool
+
+var max_enemies: int
 
 #region Godot Lifecycle
 
 func _ready():
+	_set_max_enemies()
 	_spawn_enemies()
 	
 
@@ -13,8 +18,15 @@ func _ready():
 
 #region Implementation
 
+func _set_max_enemies():
+	var map_size = Provider.get_map_size()
+	var max = (map_size.x * map_size.y) / enemy_ratio
+	
+	max_enemies = enemy_count if enemy_count_override else max
+	
+
 func _spawn_enemies():
-	for i in enemy_count:
+	for i in max_enemies:
 		var coord = Provider.get_random_empty_tile()
 		var enemy = enemie_scenes.pick_random().instantiate()
 		enemy.position = Vector3(coord.x, 0, coord.y)
